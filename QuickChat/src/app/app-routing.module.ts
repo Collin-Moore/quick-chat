e6ab3b0a682f6ca6d2c8e0e005df77f050c8077b
Collin Moore <collin.moore96@gmail.com>
2017-07-08 10:59:36 -0600

    app routing

--- QuickChat/src/app/app-routing.module.ts
+++ QuickChat/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from "app/+main/main.component";
+import { SigninComponent } from "app/+signin/signin.component";
+import { MypostsComponent } from "app/+myposts/myposts.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+    { path: '', pathMatch: 'full', component: MainComponent },
+    { path: 'signin', pathMatch: 'full', component: SigninComponent },
+    { path: 'myposts', pathMatch: 'full', component: MypostsComponent },
+    { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
