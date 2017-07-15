a8a302f6b664aee4dde8ef43f395ea6435adaa43
Collin Moore <collin.moore96@gmail.com>
2017-07-15 09:45:24 -0600

    data model schema (classes)

---
+++ QuickChat/src/app/models/firebase-flat-snapshot.ts
@@ -0,0 +1,10 @@
+export class FirebaseFlatSnapshot {
+    public $key: string;
+
+    constructor(obj?: any) {
+        if (obj && obj.$key) {
+            this.$key = obj.$key;
+        }
+    }
+
+}
