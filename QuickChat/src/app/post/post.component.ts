3cc3cd7262193cf2060a8e67cda21ac0daf5b706
Collin Moore <collin.moore96@gmail.com>
2017-07-15 15:50:39 -0600

    display edit and delete buttons when appropriate

--- QuickChat/src/app/post/post.component.ts
+++ QuickChat/src/app/post/post.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post, PostWithAuthor } from "app/models/post";
+import { AuthService } from "app/services/auth.service";
+
+enum EditMode {
+  notEditable = 0,
+  displayEditButtons = 1,
+  editing = 2,
+}
+
 
 @Component({
   selector: 'app-post',
@@ -9,9 +17,24 @@ import { Post, PostWithAuthor } from "app/models/post";
 export class PostComponent implements OnInit {
   @Input() postWithAuthor: PostWithAuthor;
 
-  constructor() { }
+  public editingMode: EditMode;
+
+  constructor(private authService: AuthService) {
+    this.editingMode = EditMode.notEditable;
+  }
 
   ngOnInit() {
+    if (this.postWithAuthor.authorKey == this.authService.currentUserUid) {
+      this.editingMode = EditMode.displayEditButtons;
+    }
+  }
+
+  enableEditing() {
+    console.log("TODO: enable editing");
+  }
+
+  remove() {
+    console.log("TODO: remove post");
   }
 
 }
